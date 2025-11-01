73dbe8cc70a998f8d1acdaa89ceac8b7cd678c1c
abdalrahman <abdalhadialbiek@gmail.com>
2025-11-01 18:00:22 +0300

    push again

--- src/common/decorator/user-role.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '../types/enum';
-
-// Roles Method
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
--- src/common/types/type.ts
+++
@@ -1,9 +0,0 @@
-export type JwtPayloadType = {
-  id: string;
-  sub: string;
-  role: string;
-};
-
-export type AccessTokenType = {
-  accessToken: string;
-};
